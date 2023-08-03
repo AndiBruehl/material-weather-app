c6c6d63039974e21394581af07fb851b7a4bc1bd
AndiBruehl <127749678+AndiBruehl@users.noreply.github.com>
2023-08-03 11:17:59 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_KEY = '384e601cb35001ae545fd783e37b5db0'; // Your OpenWeatherMap API key
+const API_KEY = '384e601cb35001ae545fd783e37b5db0'; // Dein OpenWeatherMap API-Schlüssel
 
 const App = () => {
   const [city, setCity] = useState('');
@@ -22,33 +22,42 @@ const App = () => {
     try {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
       const response = await axios.get(url);
-      setWeatherData((prevWeatherData) => [...prevWeatherData, response.data]);
+      setWeatherData((prevWeatherData) => {
+        const newWeatherData = [...prevWeatherData, response.data];
+        if (newWeatherData.length > 3) {
+          newWeatherData.shift(); // Entferne die erste Stadt, wenn mehr als 3 Städte vorhanden sind
+        }
+        return newWeatherData;
+      });
       setError(null);
       setCity('');
     } catch (error) {
       setError('Fehler beim Abrufen der Wetterdaten: ' + error.message);
-      setWeatherData([]);
     }
   };
 
+  console.log('Stadt:', city); // Zeige die eingegebene Stadt in der Konsole an
+  console.log('WeatherData:', weatherData); // Zeige das aktuelle Wetterdaten-Array in der Konsole an
+  console.log('Fehler:', error); // Zeige den aktuellen Fehler in der Konsole an
+
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <h1>Wetter App</h1>
       <input
         type="text"
         value={city}
         onChange={handleInputChange}
         onKeyPress={handleInputKeyPress}
-        placeholder="Enter city name"
+        placeholder="Stadtnamen eingeben"
       />
-      <button onClick={getWeatherData}>Get Weather</button>
+      <button onClick={getWeatherData}>Wetter abrufen</button>
 
       {weatherData.map((data, index) => (
         <div key={index}>
-          <h2>Weather data for {data.name}</h2>
-          <p>Temperature: {data.main.temp} °C</p>
-          <p>Humidity: {data.main.humidity} %</p>
-          <p>Weather description: {data.weather[0].description}</p>
+          <h2>Wetterdaten für {data.name}</h2>
+          <p>Temperatur: {data.main.temp.toFixed(1)} °C</p>
+          <p>Luftfeuchtigkeit: {data.main.humidity} %</p>
+          <p>Wetterbeschreibung: {data.weather[0].description}</p>
         </div>
       ))}
 
