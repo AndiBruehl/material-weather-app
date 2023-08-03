286548f0782e9c490ddd6879864e191bfea3e841
AndiBruehl <127749678+AndiBruehl@users.noreply.github.com>
2023-08-03 10:49:50 +0200

    multiple citys support

--- src/App.js
+++ src/App.js
@@ -5,22 +5,29 @@ const API_KEY = '384e601cb35001ae545fd783e37b5db0'; // Your OpenWeatherMap API k
 
 const App = () => {
   const [city, setCity] = useState('');
-  const [weatherData, setWeatherData] = useState(null);
+  const [weatherData, setWeatherData] = useState([]);
   const [error, setError] = useState(null);
 
   const handleInputChange = (e) => {
     setCity(e.target.value);
   };
 
+  const handleInputKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      getWeatherData();
+    }
+  };
+
   const getWeatherData = async () => {
     try {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
       const response = await axios.get(url);
-      setWeatherData(response.data);
+      setWeatherData((prevWeatherData) => [...prevWeatherData, response.data]);
       setError(null);
+      setCity('');
     } catch (error) {
       setError('Fehler beim Abrufen der Wetterdaten: ' + error.message);
-      setWeatherData(null);
+      setWeatherData([]);
     }
   };
 
@@ -31,18 +38,19 @@ const App = () => {
         type="text"
         value={city}
         onChange={handleInputChange}
+        onKeyPress={handleInputKeyPress}
         placeholder="Enter city name"
       />
       <button onClick={getWeatherData}>Get Weather</button>
 
-      {weatherData && (
-        <div>
-          <h2>Weather data for {city}</h2>
-          <p>Temperature: {weatherData.main.temp} °C</p>
-          <p>Humidity: {weatherData.main.humidity} %</p>
-          <p>Weather description: {weatherData.weather[0].description}</p>
+      {weatherData.map((data, index) => (
+        <div key={index}>
+          <h2>Weather data for {data.name}</h2>
+          <p>Temperature: {data.main.temp} °C</p>
+          <p>Humidity: {data.main.humidity} %</p>
+          <p>Weather description: {data.weather[0].description}</p>
         </div>
-      )}
+      ))}
 
       {error && <p>{error}</p>}
     </div>
